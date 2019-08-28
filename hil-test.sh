ea2238ecd233b274c5deb48d3a912bb3b1fc13ee
Murdock <riot-ci@ci.riot-os.org>
2019-08-28 10:03:38 +0200

    hil: update scripts

--- hil-test.sh
+++ hil-test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-BASE_REPO=https://github.com/RIOT-OS/RIOT
+BASE_REPO=https://github.com/RIOT-OS/RobotFW-tests
 BASE_DIR=/tmp/RIOT.hil
-HIL_REPO=https://github.com/smlng/RIOT
-HIL_BRANCH=pr/robot/i2c
 COMMIT=$1
 [ -z "$COMMIT" ] && {
     COMMIT=master
@@ -12,12 +10,10 @@ echo "COMMIT/BRANCH=${COMMIT}"
 git config --global user.email "$(hostname)@ci.riot-os.org"
 git config --global user.name "$(hostname)"
 rm -rf ${BASE_DIR}
-[ ! -d ${BASE_DIR} ] && {
-    git clone ${BASE_REPO} ${BASE_DIR}
-}
-cd ${BASE_DIR}
-git fetch --all
+git clone --recursive ${BASE_REPO} ${BASE_DIR}
+cd ${BASE_DIR}/RIOT
 git checkout -f ${COMMIT}
-git pull -f
-git pull -f --no-commit ${HIL_REPO} ${HIL_BRANCH}
+cd ${BASE_DIR}
 BUILD_DIR=${BASE_DIR}/build/${COMMIT} PHILIP_PORT=/dev/ttyAMA0 make -C tests/periph_i2c/ flash robot-test
+BUILD_DIR=${BASE_DIR}/build/${COMMIT} PHILIP_PORT=/dev/ttyAMA0 make -C tests/periph_uart/ flash robot-test
+BUILD_DIR=${BASE_DIR}/build/${COMMIT} PHILIP_PORT=/dev/ttyAMA0 make -C tests/xtimer_cli/ flash robot-test
